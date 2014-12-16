d587a3c4d397e34a79b7d4eda8f1584297da6c5c
daveztong <daveztong@gmail.com>
2014-12-16 22:09:17 +0800

    add gestureLockerView

--- app/src/main/java/me/twmm/twdroid/MainActivity.java
+++ app/src/main/java/me/twmm/twdroid/MainActivity.java
@@ -4,36 +4,46 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import me.twmm.twdroid.views.GestureLockViewGroup;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private GestureLockViewGroup mGestureLockViewGroup;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        mGestureLockViewGroup = (GestureLockViewGroup) findViewById(R.id.id_gestureLockViewGroup);
+        mGestureLockViewGroup.setAnswer(new int[] { 1, 2, 3, 4,5 });
+        mGestureLockViewGroup
+                .setOnGestureLockViewListener(new GestureLockViewGroup.OnGestureLockViewListener()
+                {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+                    @Override
+                    public void onUnmatchedExceedBoundary()
+                    {
+                        Toast.makeText(MainActivity.this, "错误5次...",
+                                Toast.LENGTH_SHORT).show();
+                        mGestureLockViewGroup.setUnMatchExceedBoundary(5);
+                    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                    @Override
+                    public void onGestureEvent(boolean matched)
+                    {
+                        Toast.makeText(MainActivity.this, matched+"",
+                                Toast.LENGTH_SHORT).show();
+                    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+                    @Override
+                    public void onBlockSelected(int cId)
+                    {
+                    }
+                });
     }
 }
